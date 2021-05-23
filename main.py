74d8f671c15f3c8d0e7033241b546a582a4a3e62
Pedro Tomé <81195750+pedrotome1981@users.noreply.github.com>
2021-05-23 12:57:20 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,46 @@
+from sqlalchemy_pagination import paginate
+from flask import Flask, render_template, request, redirect
+from sqla_wrapper import SQLAlchemy
+import os
+
+app = Flask(__name__)
+db_url = os.getenv("DATABASE_URL", "sqlite:///db.sqlite").replace("postgres://", "postgresql://", 1)
+db = SQLAlchemy(db_url)
+
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    author = db.Column(db.String, unique=False)
+    text = db.Column(db.String, unique=False)
+
+
+db.create_all()
+
+
+@app.route("/", methods=["GET"])
+def index():
+    page = request.args.get("page")
+
+    if not page:
+        page = 1
+
+    messages_query = db.query(Message)
+
+    messages = paginate(query=messages_query, page=int(page), page_size=5)
+
+    return render_template("index.html", messages=messages)
+
+
+@app.route("/add-message", methods=["POST"])
+def add_message():
+    username = request.form.get("username")
+    text = request.form.get("text")
+
+    message = Message(author=username, text=text)
+    message.save()
+
+    return redirect("/")
+
+
+if __name__ == "__main__":
+    app.run(use_reloader=True)
